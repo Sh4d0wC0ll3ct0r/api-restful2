c24dc5764c00aca43f9e06619dc7d9130f496b56
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2017-05-22 04:06:38 -0500

    se agregaron el control de accesos HTTP llamado  CORS metodos

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ var  express = require('express');
 var bodyParser =  require('body-parser');
 var app = express();
 var api = require("./routes/favorito");
-
-
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Allow', 'GET,PUT,POST,DELETE,OPTIONS');
+    next();
+});
 app.use('/api',api);
 
 
